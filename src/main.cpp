17423741ffc917ef7e2c5c9583c4d24817259693
oatpt <75560254+oatpt@users.noreply.github.com>
2022-10-03 13:53:01 +0700

    mush

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,48 @@
 #include <HardwareSerial.h>
 #include <ModbusMaster.h>
 #include <WiFi.h>
-#include <PubSubClient.h>  
-#include <Wire.h> 
+#include <PubSubClient.h>
+#include <Wire.h>
 #include "ETT_PCF8574.h"
 #include <string>
 //=================================================================================================
-#define SerialDebug           Serial                                                              
+#define SerialDebug Serial
 //=================================================================================================
-#define SerialRS485_RX_PIN    26
-#define SerialRS485_TX_PIN    27
-#define SerialRS485           Serial2                                                             
+#define SerialRS485_RX_PIN 26
+#define SerialRS485_TX_PIN 27
+#define SerialRS485 Serial2
 //=================================================================================================
-#define I2C_SCL_PIN           22                                                                  
-#define I2C_SDA_PIN           21   
+#define I2C_SCL_PIN 22
+#define I2C_SDA_PIN 21
 //=================================================================================================
-ETT_PCF8574 exp_i2c_io(PCF8574A_ID_DEV0); 
-#define RelayOn               LOW                                                                 // On Relay Ative(LOW)
-#define RelayOff              HIGH    
-#define ry3_onboard_pin       6 
-#define OnRelay()            exp_i2c_io.writePin(ry3_onboard_pin, RelayOn)                  // Open  = ON  Relay
-#define OffRelay()           exp_i2c_io.writePin(ry3_onboard_pin, RelayOff)                 // Close = OFF Relay
-#define ToggleRelay()        exp_i2c_io.writePin(ry3_onboard_pin, !exp_i2c_io.readPin(ry3_onboard_pin))   // Close = OFF Relay
-#define ReadRelay()          !exp_i2c_io.readPin(ry3_onboard_pin) 
+ETT_PCF8574 exp_i2c_io(PCF8574A_ID_DEV0);
+#define RelayOn LOW // On Relay Ative(LOW)
+#define RelayOff HIGH
+#define ry3_onboard_pin 6 //relay 3
+#define ry2_onboard_pin 4 //relay 2
+#define ry1_onboard_pin 2 //relay 1
+int relay_onboard_pin[4] = {0, ry1_onboard_pin, ry2_onboard_pin, ry3_onboard_pin};
+#define OnRelay(r) exp_i2c_io.writePin(relay_onboard_pin[r], RelayOn)                                  // Open  = ON  Relay
+#define OffRelay(r) exp_i2c_io.writePin(relay_onboard_pin[r], RelayOff)                                // Close = OFF Relay
+#define ToggleRelay(r) exp_i2c_io.writePin(relay_onboard_pin[r], !exp_i2c_io.readPin(ry3_onboard_pin)) // Close = OFF Relay
+#define ReadRelay(r) !exp_i2c_io.readPin(relay_onboard_pin[r])
 //=================================================================================================
-#define modbusSensor_SlaveID      1                                                                    
+#define modbusSensor_SlaveID 1
 //=================================================================================================
 #define MSG_BUFFER_SIZE (500)
 //=================================================================================================
-const char* ssid = "Oatpt";
-const char* password = "23456789";
+const char *ssid = "CE-ESL";
+const char *password = "ceeslonly";
+const char *mqtt_broker = "139.59.242.154";
+const char *mqtt_username = "esl";
+const char *mqtt_password = "esl603";
+const int mqtt_port = 1883;
 //=================================================================================================
-const char* mqtt_broker = "139.59.242.154";
-const char* mqtt_username = "esl";
-const char* mqtt_password = "esl603";
-const int   mqtt_port = 1883;
-//=================================================================================================                                                        
-ModbusMaster nodeSensorWeather;                                                         
-WiFiClient   espClient;
+ModbusMaster nodeSensorWeather;
+WiFiClient espClient;
 PubSubClient client(espClient);
-//=================================================================================================
 uint8_t read_modbus_status;
+String text;
 //=================================================================================================
 unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
@@ -48,17 +50,21 @@ int value = 0;
 float temperature;
 float humidity;
 unsigned long lastGetModbusTime = 0;
-char temperature_text[10];   
-char humidity_text[10];                                                                         
+char temperature_text[10];
+char humidity_text[10];
+int settemp;
+int sethumi;
+int tempInteger;
 //=================================================================================================
-void setup_wifi() {
+void setup_wifi()
+{
   delay(10);
   SerialDebug.println();
   SerialDebug.print("Connecting to ");
   SerialDebug.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) 
+  while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     SerialDebug.println("Connecting to WiFi...");
@@ -69,38 +75,54 @@ void setup_wifi() {
   SerialDebug.println(WiFi.localIP());
 }
 //=================================================================================================
-String text;
-void callback(char *topic, byte *payload, unsigned int length) {
-  for (int i = 0; i < length; i++) 
-     text+=(char) payload[i];
-  //SerialDebug.println(text);
-  //SerialDebug.println(topic);
-  //SerialDebug.println(strcmp(topic,"controlhumi"));
-  //SerialDebug.println(text.compareTo("true"));
-  //SerialDebug.println("+++++++++++++++++++++++++++++++++");
-  if(strcmp(topic,"controlhumi")==0)
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  SerialDebug.println(topic);
+  if (strcmp(topic, "els/mushroom1/controlhumi") == 0)
   {
-
-    if(text.compareTo("true")==0)
-      OnRelay();
-    else
-      OffRelay();
+    tempInteger = 0;
+    for (int i = 0; i < length; i++)
+    {
+      tempInteger *= 10;
+      tempInteger += (char)payload[i] - 48;
+    }
+    sethumi=tempInteger;
   }
-  text="";
-
+  else if (strcmp(topic, "els/mushroom1/controltemp") == 0)
+  {
+    tempInteger = 0;
+    for (int i = 0; i < length; i++)
+    {
+      tempInteger *= 10;
+      tempInteger += (char)payload[i] - 48;
+    }
+    settemp=tempInteger;
+  }
+  else if (strcmp(topic, "els/mushroom1/controllight") == 0)
+  {
+    text = "";
+    for (int i = 0; i < length; i++)
+      text += (char)payload[i];
+    if (text.compareTo("true") == 0)
+      OnRelay(1);
+    else
+      OffRelay(1);
+  }
 }
 //=================================================================================================
-void reconnect() {
+void reconnect()
+{
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
-  while (!client.connected()) 
+  while (!client.connected())
   {
     String client_id = "esp32-client-";
     client_id += String(WiFi.macAddress());
     Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) 
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
       Serial.println("Public emqx mqtt broker connected");
-    else {
+    else
+    {
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
@@ -108,61 +130,72 @@ void reconnect() {
   }
 }
 //=================================================================================================
-void setup() 
+void setup()
 {
-  Wire.begin(I2C_SDA_PIN,I2C_SCL_PIN);                                                      
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
   exp_i2c_io.begin(0xFF);
-  OffRelay();
+  for (int i = 1; i < 4; i++)
+    OffRelay(i);
   //===============================================================================================
   Serial.begin(9600);
   SerialDebug.begin(9600);
-  while(!SerialDebug);
+  while (!SerialDebug);
   //===============================================================================================
   setup_wifi();
   reconnect();
   //===============================================================================================
   SerialRS485.begin(9600, SERIAL_8N1, SerialRS485_RX_PIN, SerialRS485_TX_PIN);
-  while(!SerialRS485);
+  while (!SerialRS485);
   //===============================================================================================
   SerialDebug.println("ET-ESP32(WROVER)RS485 V3.....Start");
   //===============================================================================================
-  nodeSensorWeather.begin(modbusSensor_SlaveID, SerialRS485);                                     
+  nodeSensorWeather.begin(modbusSensor_SlaveID, SerialRS485);
   //===============================================================================================
   SerialDebug.println("ET-ESP32(WROVER)RS485 V3.....Ready");
   SerialDebug.println();
   //===============================================================================================
-  client.subscribe("controltemp");
-  client.subscribe("controlhumi");
+  client.subscribe("els/mushroom1/controltemp");
+  client.subscribe("els/mushroom1/controlhumi");
+  client.subscribe("els/mushroom1/controllight");
+  settemp=nodeSensorWeather.getResponseBuffer(0) / 10;
+  sethumi=nodeSensorWeather.getResponseBuffer(1) / 10;
   //===============================================================================================
 }
 
-void loop() 
+void loop()
 {
   //===============================================================================================
   client.loop();
-  if((millis() - lastGetModbusTime) > 5000)                                                    
+  if ((millis() - lastGetModbusTime) > 5000)
   {
-    read_modbus_status = nodeSensorWeather.readInputRegisters(0x0001, 2);                       
+    read_modbus_status = nodeSensorWeather.readInputRegisters(0x0001, 2);
     //=============================================================================================
-    if(read_modbus_status == nodeSensorWeather.ku8MBSuccess)
+    if (read_modbus_status == nodeSensorWeather.ku8MBSuccess)
     {
       //===========================================================================================
-      temperature = (float)nodeSensorWeather.getResponseBuffer(0)/10;                   
-      humidity = (float)nodeSensorWeather.getResponseBuffer(1)/10;                                             
+      temperature = (float)nodeSensorWeather.getResponseBuffer(0) / 10;
+      humidity = (float)nodeSensorWeather.getResponseBuffer(1) / 10;
       //===========================================================================================
       SerialDebug.print("Temperature = ");
-      SerialDebug.print(temperature,1);
+      SerialDebug.print(temperature, 1);
       SerialDebug.print(" C");
       SerialDebug.print(" : Humidity = ");
-      SerialDebug.print(humidity,1);
+      SerialDebug.print(humidity, 1);
       SerialDebug.print(" %");
       SerialDebug.println();
       //===========================================================================================
       sprintf(humidity_text, "%g", humidity);
       sprintf(temperature_text, "%g", temperature);
-      
-      client.publish("temp", temperature_text);
-      client.publish("humi", humidity_text);
+      client.publish("els/mushroom1/temp", temperature_text);
+      client.publish("els/mushroom1/humi", humidity_text);
+      if ((int)temperature >= settemp - 1)
+        OnRelay(2);
+      else
+        OffRelay(2);
+      if ((int)humidity <= sethumi)
+        OnRelay(3);
+      else
+        OffRelay(3);
       //===========================================================================================
     }
     lastGetModbusTime = millis();
@@ -170,5 +203,3 @@ void loop()
   }
   //===============================================================================================
 }
-
-
